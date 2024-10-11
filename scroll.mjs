530961301bcda7393228e01dca7f4ebc944aec10
Emily <126881207+hvemily@users.noreply.github.com>
2024-10-11 13:10:14 +0200

    pathing etc

--- scroll.mjs
+++ scroll.mjs
@@ -1,3 +1,7 @@
 function scrollToProjects() {
   document.getElementById('projects-title').scrollIntoView({behavior: 'smooth'});
 }
+
+function topFunction() {
+  document.getElementById('navbar').scrollIntoView({behavior: 'smooth'})
+}
