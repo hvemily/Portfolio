c5aa620ee6cdc73f6d3a8a30e20e35982b51b6ae
Emily A. Brynestad-Hagen <emily.brynestad@hotmail.com>
2024-10-12 21:56:57 +0200

    updates

--- navbar.mjs
+++ navbar.mjs
@@ -1,5 +1,5 @@
 document.getElementById('hamburger-menu').addEventListener('click', function() {
-  const menu = document.getElementById('nav-links');
+  const menu = document.getElementById('navlinks');
   menu.classList.toggle('show-menu');
   
   console.log('Current class list:', menu.classList); // For å sjekke klasser
