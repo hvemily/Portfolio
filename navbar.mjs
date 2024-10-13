e1908525c053eb350f67468bd377dd921899095b
Emily <126881207+hvemily@users.noreply.github.com>
2024-10-13 21:16:35 +0200

    update

--- navbar.mjs
+++ navbar.mjs
@@ -2,5 +2,5 @@ document.getElementById('hamburger-menu').addEventListener('click', function() {
   const menu = document.getElementById('navlinks');
   menu.classList.toggle('show-menu');
   
-  console.log('Current class list:', menu.classList); // For å sjekke klasser
+
 });
